2b3e41a60356d2171975afe734a96187a38ba6ed
JAIRAMEZCUA <enriquejairamezcuamaciasg3@outlook.com>
2020-09-24 14:00:48 -0500

    MI PRIMER COMMIT

--- app/src/main/java/com/naat/examenapp/MainActivity2.java
+++ app/src/main/java/com/naat/examenapp/MainActivity2.java
@@ -2,15 +2,16 @@ package com.naat.examenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
+    String a ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("Hola");
+        setContentView(R.layout.activity_main2);
 
     }
 }
