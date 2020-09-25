8420986d894692274ea75d12dad2012e31138ccd
Jair Amezcua <jairamezcuamacias1@gmail.com>
2020-09-25 00:21:00 -0500

    INTERFACELista

--- app/src/main/java/com/naat/examenapp/MainActivity.java
+++ app/src/main/java/com/naat/examenapp/MainActivity.java
@@ -1,27 +1,38 @@
 package com.naat.examenapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import static com.naat.examenapp.MainActivity2.TEXT_REQUEST;
+import static com.naat.examenapp.MainActivity2.listaImagenes;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button,btnsalir;
+    ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.btnpasar);
         btnsalir=findViewById(R.id.btnsalir);
+        im=findViewById(R.id.imageView);
+        if(savedInstanceState !=null){
+
+        }
 
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -62,16 +73,46 @@ public class MainActivity extends AppCompatActivity {
         toast1.show();
 
     }
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-       // img1.setText(savedInstanceState.getString("KEYSTRING"));
+    public  void cargar(){
+        SharedPreferences sharedPreferences= getSharedPreferences("llaveimagen",MODE_PRIVATE);
+        int index=sharedPreferences.getInt("imagen",0);
+        im.setImageResource(listaImagenes[index].getImageResource());
+        Toast toast1 = Toast.makeText(getApplicationContext(),index, Toast.LENGTH_SHORT);
+        toast1.setGravity(Gravity.CENTER,0,0);
+        toast1.show();
     }
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TEXT_REQUEST){
+            if(resultCode==RESULT_OK) {
+                reply = Integer.parseInt(data.getStringExtra("llaveimagen"));
+                im.setImageResource(listaImagenes[reply].getImageResource());
+
+            }
+        }
+    }
+
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        //String message=tv1.getText().toString();
-        //img1.setText(message);
-        //outState.putString("KEYSTRING",message);
+       // outState.putString("llaveimagen");
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        im.setImageResource(listaImagenes[reply].getImageResource());
+    }
+
+    public void primer(){
+        Intent intent= new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra("llaveimagen",reply);
+        //REQUEST ES PARA VER LOS PERMISOS
+        //SOLICITANDO CON UN REQUEST Y ESPERO UNA RESPUESTA
+        startActivityForResult(intent,TEXT_REQUEST);
+    }
+
+     */
 }
--- app/src/main/java/com/naat/examenapp/MainActivity2.java
+++ app/src/main/java/com/naat/examenapp/MainActivity2.java
@@ -3,19 +3,25 @@ package com.naat.examenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.naat.examenapp.Datos.Imagenabs;
 
 public class MainActivity2 extends AppCompatActivity {
-    private Imagenabs [] listaImagenes = {
+    public static  final int TEXT_REQUEST=1;
+
+    public static Imagenabs [] listaImagenes = {
             new Imagenabs() {
                 @Override
                 public int getImageResource() {
@@ -37,12 +43,12 @@ public class MainActivity2 extends AppCompatActivity {
     };
 
 
-    Button btnpersona,btncarro,btndinero;
-    ImageButton btn1,btn2,btn3;
+    Button btnpersona,btncarro,btndinero,btnregresa;
+    ImageButton btn1,btn2,btn3,btn4;
     ImageView imageView;
     TextView textView;
     private Typeface tipo;
-
+    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,23 +66,30 @@ public class MainActivity2 extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     textView.setText(R.string.persona);
-                    imageView.setImageResource(listaImagenes[0].getImageResource());
+                    index=0;
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
             btn2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     textView.setText(R.string.carro);
-                    imageView.setImageResource(listaImagenes[1].getImageResource());
+                    index=1;
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
             btn3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    index=2;
                     textView.setText(R.string.dinero);
-                    imageView.setImageResource(listaImagenes[2].getImageResource());
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
+
         }else{
 
             imageView=findViewById(R.id.imageView3);
@@ -89,27 +102,40 @@ public class MainActivity2 extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     textView.setText(R.string.persona);
-                    imageView.setImageResource(listaImagenes[0].getImageResource());
+                    index=0;
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
             btncarro.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    index=1;
                     textView.setText(R.string.carro);
-                    imageView.setImageResource(listaImagenes[1].getImageResource());
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
             btndinero.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    index=2;
                     textView.setText(R.string.dinero);
-                    imageView.setImageResource(listaImagenes[2].getImageResource());
+                    imageView.setImageResource(listaImagenes[index].getImageResource());
+                    guarda();
                 }
             });
-
         }
+
         String fuente = "Fuentes/nuevo.ttf";
         this.tipo = Typeface.createFromAsset(getAssets(),fuente);
         textView.setTypeface(tipo);
+    }
+
+    public void guarda (){
+        SharedPreferences sharedPreferences=getSharedPreferences("llaveimagen",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putInt("imagen",index);
+        editor.commit();
     }
 }
